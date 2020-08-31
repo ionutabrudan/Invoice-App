683a94d27c2a0334e9003ca2669d71d6b1f371ae
ionut abrudan <ionutabrudan@icloud.com>
2020-08-31 21:44:08 +0300

    Initial project setup

---
+++ InvoiceApp/InvoiceApp/Models/Customer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvoiceApp.Models
+{
+    public class Customer
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string CustomerCode { get; set; }
+
+        [Required]
+        [Display(Name = "Customer Type")]
+        public CustomerType CustomerType { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Web Adress")]
+        public string Web { get; set; }
+
+        [StringLength(100)]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Adress")]
+        public string Email { get; set; }
+
+        [StringLength(20)]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+    }
+}
---
+++ InvoiceApp/InvoiceApp/Models/CustomerType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvoiceApp.Models
+{
+    public enum CustomerType
+    {
+       
+        PFA = 0,
+        
+        Srl
+    }
+}
